911f34b9e2394577639195b52bd5b95ec72fd33f
Tilman Potthof <tilmanpotthof@googlemail.com>
2014-09-21 02:43:02 +0200

    Updated dependencies (angular, jQuery and bootstrap).

--- resources/build/test/karma.conf.js
+++ resources/build/test/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
     config.set({
 
         files: [
-            '../bower_components/jquery/jquery.js',
+            '../bower_components/jquery/dist/jquery.js',
             '../bower_components/angular/angular.js',
             '../bower_components/angular-mocks/angular-mocks.js',
             '../angular-slashTools.js',
--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
         basePath: BASE,
 
         files: [
-            'bower_components/jquery/jquery.js',
+            'bower_components/jquery/dist/jquery.js',
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
             'src/**/*.js'
